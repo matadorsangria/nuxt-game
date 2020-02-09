fb2558f1c62b3697da065a75796c7e97c8da60c1
江原 圭 <matador.sangria@gmail.com>
2020-02-09 17:36:16 +0900

    Fix: Netlify対応

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,18 @@ module.exports = {
   mode: 'spa',
 
   /*
+  ** Define the project ENV variables
+  */
+  env: {
+    FB_API_KEY: process.env.FB_API_KEY,
+    FB_AUTH_DOMAIN: process.env.FB_AUTH_DOMAIN,
+    FB_DATABASE_URL: process.env.FB_DATABASE_URL,
+    FB_PROJECTID: process.env.FB_PROJECTID,
+    FB_STORAGE_BUCKET: process.env.FB_STORAGE_BUCKET,
+    FB_MESSAGING_SENDER_ID: process.env.FB_MESSAGING_SENDER_ID
+  },
+
+  /*
   ** Headers of the page
   */
   head: {
